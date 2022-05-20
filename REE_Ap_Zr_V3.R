f0ecae7d6eec212137a46f7c51ea02f1e4ee4466
Marcelo Arias <50493706+marceloariasqca@users.noreply.github.com>
2022-05-20 16:31:58 -0300

    saca el promedio y la desvStandar

--- REE_Ap_Zr_V3.R
+++ REE_Ap_Zr_V3.R
@@ -3,26 +3,30 @@ setwd("D:/Users/cronar/Desktop/Result")
 
 
 #Imports
+
 library(readr)
 library(openxlsx)
+
+
 #AGREGAR VARIABLES Chondrite (up/g) ppm
-La = 0.367  #2
-Ce = 0.957  #4
-Pr = 0.137   #6
-Nd = 0.711  #8
-Sm = 0.231  #10
-Eu = 0.087  #12
-Gd = 0.306  #14
-Tb = 0.058  #16
-Dy = 0.381  #18
-Ho = 0.0851 #20
-Er = 0.249  #22
-Tm = 0.0356 #24
-Yb = 0.248  #26
-Lu = 0.0381 #28   esto corresponde al numero de columnas dentro de Tabala_a_usar# 
+La = 0.367  
+Ce = 0.957  
+Pr = 0.137  
+Nd = 0.711  
+Sm = 0.231  
+Eu = 0.087  
+Gd = 0.306 
+Tb = 0.058  
+Dy = 0.381  
+Ho = 0.0851 
+Er = 0.249  
+Tm = 0.0356 
+Yb = 0.248  
+Lu = 0.0381  
 
 
 # buscar la ruta del archivo de csv
+
 Datos <- read.csv(file.choose()) 
 
 #normalizar Tabla_a_usar con las variables de la chondrita
@@ -45,6 +49,7 @@ Yb_ppm <- Datos$Yb..ppm/Yb
 Lu_ppm <- Datos$Lu..ppm/Lu
 
 Tabla_Normalizada<-data.frame(tabla_Normalizada <- cbind(Muestras, La_ppm,  Ce_ppm, Pr_ppm,  Nd_ppm,   Sm_ppm,  Eu_ppm,  Gd_ppm,  Tb_ppm,   Dy_ppm,  Ho_ppm, Er_ppm,  Tm_ppm, Yb_ppm, Lu_ppm))
+
 Tabla_Normalizada <- type.convert(Tabla_Normalizada)
 
 
@@ -67,16 +72,52 @@ X1s_Yb_ppm <- Datos$X1s.Yb..ppm
 X1s_Lu_ppm <- Datos$X1s.Lu..ppm
 
 Errores <-data.frame(Errores <- cbind(Muestras, X1s_La_ppm, X1s_Ce_ppm, X1s_Pr_ppm, X1s_Nd_ppm, X1s_Sm_ppm, X1s_Eu_ppm, X1s_Gd_ppm, X1s_Tb_ppm, X1s_Dy_ppm, X1s_Ho_ppm, X1s_Er_ppm, X1s_Tm_ppm, X1s_Yb_ppm,X1s_Lu_ppm))
+
 Errores <- type.convert(Errores)
 
 #Traspone las tablas Normalizada y la de los Errores. 
 
 Normalizada_T<- data.frame(t(Tabla_Normalizada[-1]))
+
 colnames(Normalizada_T) <- Tabla_Normalizada[, 1]
 
 
 Error_T<-data.frame(t(Errores[-1]))
+
 colnames(Error_T) <- Errores [, 1]
+
+#sacar Promedio y DesEst de la tabla Normalizada_T
+
+mean_La<-mean(La_ppm)
+mean_Ce<-mean(Ce_ppm)
+mean_Pr<-mean(Pr_ppm)
+mean_Nd<-mean(Nd_ppm)
+mean_Sm<-mean(Sm_ppm)
+mean_Eu<-mean(Eu_ppm)
+mean_Gd<-mean(Gd_ppm)
+mean_Tb<-mean(Tb_ppm)
+mean_Dy<-mean(Dy_ppm)
+mean_Ho<-mean(Ho_ppm)
+mean_Er<-mean(Er_ppm)
+mean_Tm<-mean(Tm_ppm)
+mean_Yb<-mean(Yb_ppm)
+mean_Lu<-mean(Lu_ppm)
+
+sd_La<-sd(La_ppm)
+sd_Ce<-sd(Ce_ppm)
+sd_Pr<-sd(Pr_ppm)
+sd_Nd<-sd(Nd_ppm)
+sd_Sm<-sd(Sm_ppm)
+sd_Eu<-sd(Eu_ppm)
+sd_Gd<-sd(Gd_ppm)
+sd_Tb<-sd(Tb_ppm)
+sd_Dy<-sd(Dy_ppm)
+sd_Ho<-sd(Ho_ppm)
+sd_Er<-sd(Er_ppm)
+sd_Tm<-sd(Tm_ppm)
+sd_Yb<-sd(Yb_ppm)
+sd_Lu<-sd(Lu_ppm)
+
 
 
 #combinamos las dos tablas para generar la final
@@ -92,10 +133,10 @@ M7 <- Normalizada_T[, 7]
 M8 <- Normalizada_T[, 8]
 M9 <- Normalizada_T[, 9]
 M10 <- Normalizada_T[, 10]
-#M11 <- Normalizada_T[, 11]
-#M12 <- Normalizada_T[, 12]
-#M13 <- Normalizada_T[, 13]
-#M14 <- Normalizada_T[, 14]
+M11 <- Normalizada_T[, 11]
+M12 <- Normalizada_T[, 12]
+M13 <- Normalizada_T[, 13]
+M14 <- Normalizada_T[, 14]
 
 
 Error1 <- Error_T[, 1]
@@ -108,15 +149,23 @@ Error7 <- Error_T[, 7]
 Error8 <- Error_T[, 8]
 Error9 <- Error_T[, 9]
 Error10 <- Error_T[, 10]
-#Error11 <- Error_T[, 11]
-#Error12 <- Error_T[, 12]
-#Error13 <- Error_T[, 13]
-#Error14 <- Error_T[, 14]
+Error11 <- Error_T[, 11]
+Error12 <- Error_T[, 12]
+Error13 <- Error_T[, 13]
+Error14 <- Error_T[, 14]
 
 REE <- c("La", "Ce", "Pr", "Nd", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu")
 
-Final <- data.frame( REE, M1 , Error1, M2 , Error2, M3 , Error3, M4 , Error4, M5 , Error5, M6 , Error6, M7 , Error7, M8 , Error8, M9 , Error9, M10 , Error10)#, M11 , Error11, M12 , Error12, M13 , Error13, M14 , Error14)
+Promedio <- c (mean_La, mean_Ce, mean_Pr, mean_Nd, mean_Sm, mean_Eu, mean_Gd, mean_Tb, mean_Dy, mean_Ho, mean_Er, mean_Tm, mean_Yb, mean_Lu)
+
+DesviacionST <- c(sd_La, sd_Ce, sd_Pr, sd_Nd, sd_Sm, sd_Eu, sd_Gd, sd_Tb, sd_Dy, sd_Ho, sd_Er, sd_Tm, sd_Yb, sd_Lu)
+
+
+Final <- data.frame( REE, M1 , Error1, M2 , Error2, M3 , Error3, M4 , Error4, M5 , Error5, M6 , Error6, M7 , Error7, M8 , Error8, M9 , Error9, M10 , Error10, M11 , Error11, M12 , Error12, M13 , Error13, M14 , Error14, Promedio, DesviacionST)
+
 Final <- type.convert(Final)
+
+
 
 #escribe el archivo final. 
 #write.csv(Tabla_Normalizada, "D:/Users/cronar/Desktop/Result/Normalizada_REE.csv",row.names=FALSE)
